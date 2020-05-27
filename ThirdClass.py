54e3aabe9b0001c97d067f65ff3b0e5c656b6843
Rapha-Y <raph.yuki@gmail.com>
2020-05-27 17:14:47 -0300

    added conditional blocks

---
+++ ThirdClass.py
@@ -0,0 +1,28 @@
+# a = int(input("Primeiro valor: "))
+# b = int(input("Segundo valor: "))
+# c = int(input("Terceiro valor: "))
+#
+# if a > b and a > c:
+#     print("O maior número é {}".format(a))
+# elif b > a and b > c:
+#     print("O maior número é {}".format(b))
+# else:
+#     print("O maior número é {}".format(c))
+#
+# print("Final do programa")
+
+# a = int(input("Entre com o primeiro valor: "))
+# b = int(input("Entre com o segundo valor: "))
+# rest_a = a % 2
+# rest_b = b % 2
+# if rest_a == 0 or not rest_b > 0:
+#     print("Foi digitado um número par")
+# else:
+#     print("Nenhum número par foi digitado")
+
+a = int(input("Primeiro bimestre: "))
+b = int(input("Segundo bimestre: "))
+c = int(input("Terceiro bimestre: "))
+d = int(input("Quarto bimestre: "))
+mean = (a + b + c + d) / 4
+print("Média: {}".format(mean))
