5510975400df104e4e700c5c1916ca3a0f2b5f40
Rapha-Y <raph.yuki@gmail.com>
2020-05-27 20:29:36 -0300

    added array/tuple manipulation

---
+++ FifthClass.py
@@ -0,0 +1,28 @@
+numbers = [1, 3, 5, 7]
+animals = ["Cachorro", "Gato", "Rato", "Chinchila"]
+
+tup = (1, 10, 12, 16)
+print(tup)
+print(len(tup))
+
+animal_tuple = tuple(animals)
+print(animal_tuple)
+
+num_list = list(tup)
+print(num_list)
+
+# animals.sort()
+# print(animals)
+# animals.reverse()
+# print(animals)
+
+# if "Coelho" in animals:
+#     print("Tem um coelho na lista")
+# else:
+#     print("Cadê o Sansão?")
+#     animals.append("Coelho")
+#     print(animals)
+
+# animals.pop(2)
+# animals.remove("Cachorro")
+# print(animals)
